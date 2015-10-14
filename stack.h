6b1392551361a45b202f475a2ad67867e2a969d4
TennyZhuang <zhuangtianyi0826@163.com>
2015-10-14 10:53:34 +0800

    stack

---
+++ stack.h
@@ -0,0 +1,21 @@
+#include "vector.h"
+
+const size_t DEFAULT_CAPACITY = 100;
+
+template <typename T>
+class Stack: public Vector<T> {
+public:
+    Stack() { }
+
+    inline void push(T d) {
+        Vector<T>::push_back(d);
+    }
+
+    inline T pop() {
+        return Vector<T>::pop_back();
+    }
+
+    inline T& top() {
+        return Vector<T>::back();
+    }
+};
