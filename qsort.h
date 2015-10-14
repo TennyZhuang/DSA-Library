9784cf44e6636c3e7842dca7511c5e7b0f48aa54
TennyZhuang <zhuangtianyi0826@163.com>
2015-10-14 10:53:00 +0800

    qsort

---
+++ qsort.h
@@ -0,0 +1,22 @@
+template <typename T>
+inline void swap(T &a, T &b) {
+    T t = b;
+    b = a;
+    a = t;
+}
+
+template <typename T>
+void qsort(T *A, int lo, int hi) {
+    if (lo >= hi)
+        return;
+
+    int m = lo;
+    for (int i = lo + 1; i <= hi; i++)
+        if (A[i] < A[lo])
+            swap<T>(A[++m], A[i]);
+
+    swap(A[lo], A[m]);
+
+    qsort(A, lo, m - 1);
+    qsort(A, m + 1, hi);
+}
